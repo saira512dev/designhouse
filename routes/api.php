00973784756210af2e606e4cefc3283e77533c81
saira <saira@ceribs.com>
2020-10-01 20:24:07 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Routes for authenticated users only
+
+Route::group(['middleware' => ['auth:api']],function(){
+
+});
+
+//Routes for guests only
+Route::group(['middleware' => ['guest:api']], function(){
+    Route::post('register', 'Auth\RegisterController@register');
+});
+
+
+
+
