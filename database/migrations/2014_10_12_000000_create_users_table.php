1d6be605c550d44273f341d36c51a539316fcce5
saira <saira@ceribs.com>
2020-10-03 20:30:34 +0530

    new fields for registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('tagline')->nullable();
+            $table->text('about')->nullable();
+            $table->point('location')->nullable();
+            $table->string('formatted_address')->nullable();
+            $table->boolean('available_to_hire')->default(false);
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
