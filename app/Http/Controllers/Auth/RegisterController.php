33a564d2680ab79301ed09c43cc7e86dad0f0b58
saira <saira@ceribs.com>
2020-10-03 20:31:52 +0530

    code for api route and registration changed

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Supports\Facades\Hash;
 use Illuminate\Supports\Facades\Validator;
 
@@ -13,28 +12,42 @@ use Illuminate\Supports\Facades\Validator;
 
 class RegisterController extends Controller
 {
-    use AuthorizesRequests, RegistersUsers, ValidatesRequests;
 
 
-    public function __construct()
+    // public function __construct()
+    // {
+    //     //$this->middleware('guest');
+    // }
+    
+     protected function register(Request $request, User $user)
     {
-        //$this->middleware('guest');
+        return response()->json($request,'200');
     }
    
-    protected function registered(Request $request, User $user)
-    {
-        return response()->json($user,'200');
-    }
+    // protected function registered(Request $request, User $user)
+    // {
+    //     return response()->json($user,'200');
+    // }
    
-    protected function validator(array $data)
-    {
-        return validator::make($data, [
-            'username' => ['required', 'string', 'max:15', 'alpha_dash','unique:users'],
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required','string','email','max:255','unique:users'],
-            'password' => ['required', 'string', 'min:8','confirmed'],
-        ]);
-    }
+    // protected function validator(array $data)
+    // {
+    //     return validator::make($data, [
+    //         'username' => ['required', 'string', 'max:15', 'alpha_dash','unique:users,username'],
+    //         'name' => ['required', 'string', 'max:255'],
+    //         'email' => ['required','string','email','max:255','unique:users'],
+    //         'password' => ['required', 'string', 'min:8','confirmed'],
+    //     ]);
+    // }
+
+    // protected function create(array $data)
+    // {
+    //     return User::create([
+    //         'name' => $data['name'],
+    //         'username' => $data['username'],
+    //         'email' => $data['email'],
+    //         'password' => Hash::make($data['password']),
+    //     ]);
+    // }   
 
     
 }
