5048e3d69411e247b768b673360e3717dfc28006
saira <saira@ceribs.com>
2020-10-12 13:42:38 +0530

    routes for 'me' endpoint,logout and password reset added

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Public routes
+Route::get('me', 'User\MeController@getMe');
+
 //Routes for authenticated users only
 
 Route::group(['middleware' => ['auth:api']],function(){
-
+    Route::post('logout','Auth\LoginController@logout');
 });
 
 //Routes for guests only
@@ -15,6 +18,9 @@ Route::group(['middleware' => ['guest:api']], function(){
     Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend')->middleware([ 'throttle:6,1'])->name('verification.send');    
     Route::post('login', 'Auth\LoginController@authenticate');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 });
 
 
