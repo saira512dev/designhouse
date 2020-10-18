31808ca451acada76d423368cd6485834b1f7c99
saira <saira@ceribs.com>
2020-10-18 22:19:15 +0530

    AuthorizesRequest trait added

--- app/Http/Controllers/Auth/Controller.php
+++ app/Http/Controllers/Auth/Controller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
