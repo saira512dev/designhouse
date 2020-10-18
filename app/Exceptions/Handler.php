b49580359ffd0995f545045f8ff1f8267d30ee91
saira <saira@ceribs.com>
2020-10-18 22:18:33 +0530

    Exception to return errors in json format added

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Database\Eloquent\ModelNotFoundException;  
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -32,6 +35,30 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        //
+           
+   
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthorizationException) {
+            if($request->expectsJson()){
+                return response()->json([
+                    "errors" => [ "message" => "You are not authorized to access this resource"]], 403);
+            }
+            
+        }
+
+        if ($exception instanceof ModelNotFoundException && $request->expectsJson()) {
+                return response()->json([
+                    "errors" => [ "message" => " The resource was not found in the database"]], 404);
+            
+        }
+
+        return parent::render($request, $exception);
+
+    }
+
+
+    
 }
