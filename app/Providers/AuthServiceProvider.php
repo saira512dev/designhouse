d0dba4d5b95be453f7ef1d461cc8d240bb4981f0
saira <saira@ceribs.com>
2020-10-27 23:21:08 +0530

    new policy InvitationPolicy added

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Models\Team;
+use App\Models\Invitation;
 use App\Models\Design;
 use App\Policies\TeamPolicy;
 use App\Policies\DesignPolicy;
+use App\Policies\InvitationPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Team::class => TeamPolicy::class,
         Design::class => DesignPolicy::class,
+        Invitation::class => InvitationPolicy::class,
     ];
 
     /**
