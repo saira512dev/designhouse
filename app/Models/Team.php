c3609c4e909acf243a85c4b4aad4a0da449ff39e
saira <saira@ceribs.com>
2020-10-27 23:24:16 +0530

    team migration and model added

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,21 +2,23 @@
 
 namespace App\Models;
 
-use Laravel\Jetstream\Events\TeamCreated;
-use Laravel\Jetstream\Events\TeamDeleted;
-use Laravel\Jetstream\Events\TeamUpdated;
-use Laravel\Jetstream\Team as JetstreamTeam;
+// use Laravel\Jetstream\Events\TeamCreated;
+// use Laravel\Jetstream\Events\TeamDeleted;
+// use Laravel\Jetstream\Events\TeamUpdated;
+// use Laravel\Jetstream\Team as JetstreamTeam;
+use Illuminate\Database\Eloquent\Model;
 
-class Team extends JetstreamTeam
+
+class Team extends Model
 {
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
-        'personal_team' => 'boolean',
-    ];
+    // protected $casts = [
+    //     'personal_team' => 'boolean',
+    // ];
 
     /**
      * The attributes that are mass assignable.
@@ -25,17 +27,70 @@ class Team extends JetstreamTeam
      */
     protected $fillable = [
         'name',
-        'personal_team',
+        'owner_id',
+        'slug',
     ];
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        //when a team is created,add current user as team member
+        static::created(function($team){
+            $team->members()->attach(auth()->id());
+        });
+
+        static::deleting(function($team){
+            $team->members()->sync([]);
+        });
+    } 
+
+
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(User::class)
+        ->withTimestamps();
+    }
+
+    public function designs()
+    {
+        return $this->hasMany(Design::class);
+    }
+
+    public function hasUser(User $user)
+    {
+        return $this->members()
+                ->where('user_id',$user->id)
+                ->first() ? true: false;
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function hasPendingInvite($email)
+    {
+        return (bool)$this->invitations()
+                            ->where('recipient_email',$email)
+                            ->count();
+    }
 
     /**
      * The event map for the model.
      *
      * @var array
      */
-    protected $dispatchesEvents = [
-        'created' => TeamCreated::class,
-        'updated' => TeamUpdated::class,
-        'deleted' => TeamDeleted::class,
-    ];
+    // protected $dispatchesEvents = [
+    //     'created' => TeamCreated::class,
+    //     'updated' => TeamUpdated::class,
+    //     'deleted' => TeamDeleted::class,
+    // ];
 }
---
+++ database/migrations/2020_10_26_103756_create_teams_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('slug')->unique()->index();
+            $table->string('name');
+            $table->bigInteger('owner_id')->unsigned();
+            $table->timestamps();
+
+
+
+            $table->foreign('owner_id')
+                    ->references('id')
+                    ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teams');
+    }
+}
