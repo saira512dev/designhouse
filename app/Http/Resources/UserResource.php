efe7c55cb792b688a1705640c78bebaaf54bc320
saira <saira@ceribs.com>
2020-10-29 12:37:12 +0530

    condition mergewhen added

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
+
 
 class UserResource extends JsonResource
 {
@@ -17,7 +19,9 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'username' => $this->username,
-            'email' => $this->email,
+            $this->mergeWhen(Auth::check() && Auth::id() == $this->id,[
+                'email' => $this->email,
+            ]),
             'name' => $this->name,
             'designs' => DesignResource::collection(
                             $this->whenLoaded('designs')),
